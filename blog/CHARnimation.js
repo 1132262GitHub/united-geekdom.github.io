4286a5667769912853e98d51827bdb9e66a1471a
Kiran Nithilan <kirannithilan@gmail.com>
2020-04-27 19:12:12 +0530

    Naa oru makkumanda

---
+++ blog/CHARnimation.js
@@ -0,0 +1,224 @@
+function frame1() {
+var a = "";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame2() {
+var a = "&nbsp;&nbsp;o(WELCOME TO UNITED GEEKDOM)";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame3() {
+var a = "&nbsp;&nbsp;o(Awesome link | Even more awesome link | Lots of awesome stuff!!!)";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame4() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [1])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame5() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [2])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame6() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [3])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame7() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [4])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame8() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [5])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame9() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [7])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame10() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [9])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame11() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [11])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame12() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [15])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame13() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [19])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame14() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [23])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame15() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [39])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame16() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [55])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame17() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [71])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame18() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [88])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame19() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [104])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame20() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [215])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame21() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [326])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame22() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [437])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame23() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [548])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame24() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [659])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame25() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [1321])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame26() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [23213])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame27() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [332513])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame28() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [4328513])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame29() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [53284513])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame30() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [613284513])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame31() {
+var a = "&nbsp;&nbsp;o(united-geekdom.github.io: site visits [The whole world!!])";
+var b = "&nbsp;O"
+var c = "/|\\"
+var d = "/ \\"
+var e = "| |"
+}
+function frame32() {
+var a = "________";
+var b = "|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|";
+var c = "|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|";
+var d = "|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|";
+var e = "<u>|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</u>";
+}
